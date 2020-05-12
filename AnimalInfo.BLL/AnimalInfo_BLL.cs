ee5ab30a55b9520fe413b87aea3bf070643df0f5
Jônatas Mendes da Cruz <62781857+Kyoto-01@users.noreply.github.com>
2020-05-11 22:07:39 -0300

    Upload dos arquivos
    Upload dos arquivos que compõem o sistema animal _info_CRUD

---
+++ AnimalInfo.BLL/AnimalInfo_BLL.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using AnimalInfo.DAL;
+using AnimalInfo.DTO;
+
+namespace AnimalInfo.BLL
+{
+    public class AnimalInfo_BLL
+    {
+        public string enderecoFotos
+        {
+            get { return System.IO.Directory.GetCurrentDirectory() + "\\fotos\\"; }
+        }
+
+        public List<Animal> buscarAnimais()
+        {
+            try
+            {
+                return new AnimalInfo_DAL().BuscarAnimais();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int EditarAnimal(Animal animalEditado)
+        {
+            try
+            {
+                return new AnimalInfo_DAL().UpdateAnimal(animalEditado);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int AddAnimal(Animal novoAnimal)
+        {
+            try
+            {
+                return new AnimalInfo_DAL().AddAnimal(novoAnimal);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public int RemoveAnimal(Animal animalToRemove)
+        {
+            try
+            {
+                return new AnimalInfo_DAL().RemoveAnimal(animalToRemove);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
